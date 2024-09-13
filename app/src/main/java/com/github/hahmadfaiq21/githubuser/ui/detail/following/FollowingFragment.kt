e332178956d9230af297f15d563463ea65714625
Husain Ahmad Faiq <hahmadfaiq@gmail.com>
2024-09-13 15:43:22 +0700

    Adding Intent data in FollowersFragment and FollowingFragment

--- app/src/main/java/com/github/hahmadfaiq21/githubuser/ui/detail/following/FollowingFragment.kt
+++ app/src/main/java/com/github/hahmadfaiq21/githubuser/ui/detail/following/FollowingFragment.kt
@@ -1,11 +1,13 @@
 package com.github.hahmadfaiq21.githubuser.ui.detail.following
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.hahmadfaiq21.githubuser.R
+import com.github.hahmadfaiq21.githubuser.data.response.UserResponse
 import com.github.hahmadfaiq21.githubuser.databinding.FragmentFollowBinding
 import com.github.hahmadfaiq21.githubuser.ui.adapter.UserAdapter
 import com.github.hahmadfaiq21.githubuser.ui.detail.DetailUserActivity
@@ -20,11 +22,24 @@ class FollowingFragment : Fragment(R.layout.fragment_follow) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val args = arguments
         username = args?.getString(DetailUserActivity.EXTRA_USERNAME).toString()
 
         _binding = FragmentFollowBinding.bind(view)
+
         adapter = UserAdapter()
+        adapter.setOnItemClickCallback(object : UserAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: UserResponse) {
+                Intent(activity, DetailUserActivity::class.java).also {
+                    it.putExtra(DetailUserActivity.EXTRA_USERNAME, data.login)
+                    it.putExtra(DetailUserActivity.EXTRA_ID, data.id)
+                    it.putExtra(DetailUserActivity.EXTRA_URL, data.avatarUrl)
+                    startActivity(it)
+                }
+            }
+        })
+
         binding.apply {
             rvUser.setHasFixedSize(true)
             rvUser.layoutManager = LinearLayoutManager(activity)
